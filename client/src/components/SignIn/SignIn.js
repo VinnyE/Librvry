a4c2ba41f7f535746f5fe0741046228242da3b0c
Vinny <espinalv1@gmail.com>
2018-06-18 06:44:12 -0400

    Added validation to SignIn form.

--- client/src/components/SignIn/SignIn.js
+++ client/src/components/SignIn/SignIn.js
@@ -1,29 +1,85 @@
 import React, { Component } from "react";
+import isEmail from "validator/lib/isEmail";
 
 import styles from "./SignIn.module.css";
 
 class SignIn extends Component {
   state = {
     fields: {
-      username: "",
+      email: "",
       password: ""
+    },
+    touched: {
+      email: false,
+      password: false
     }
   };
 
   handleSubmit = () => {};
 
+  validate = (email, password) => {
+    const { touched } = this.state;
+    const errors = {
+      email: false,
+      password: false
+    };
+
+    if (email.length === 0 && touched.email) {
+      errors.email = "Email is required.";
+    }
+
+    if (!isEmail(email) && touched.email) {
+      errors.email = "A valid email address is required.";
+    }
+
+    if (password.length === 0 && touched.password) {
+      errors.password = "Password is required.";
+    }
+
+    return errors;
+  };
+
+  handleBlur = field => evt => {
+    this.setState({
+      touched: { ...this.state.touched, [field]: true }
+    });
+  };
+
+  handleEmailChange = evt => {
+    this.setState({
+      fields: { ...this.state.fields, email: evt.target.value }
+    });
+  };
+
+  handlePasswordChange = evt => {
+    this.setState({
+      fields: { ...this.state.fields, password: evt.target.value }
+    });
+  };
+
   render() {
+    const errors = this.validate(
+      this.state.fields.email,
+      this.state.fields.password
+    );
+    const hasErrors = Object.keys(errors).some(val => errors[val]);
+
     return (
       <form onSubmit={this.handleSubmit} className={styles.form}>
-        <label className={styles.label} htmlFor="username">
-          Username:
+        <label className={styles.label} htmlFor="email">
+          Email:
           <input
             type="text"
             placeholder="example@example.com"
-            id="username"
+            id="email"
             className={styles.textInput}
-            value={this.state.username}
+            value={this.state.email}
+            onBlur={this.handleBlur("email")}
+            onChange={this.handleEmailChange}
           />
+          {errors.email && (
+            <span className={styles.errorMsg}>{errors.email}</span>
+          )}
         </label>
         <label className={styles.label} htmlFor="password">
           Password:
@@ -33,9 +89,19 @@ class SignIn extends Component {
             id="password"
             className={styles.textInput}
             value={this.state.password}
+            onBlur={this.handleBlur("password")}
+            onChange={this.handlePasswordChange}
           />
+          {errors.password && (
+            <span className={styles.errorMsg}>{errors.password}</span>
+          )}
         </label>
-        <input type="submit" value="Log In" className={styles.signInBtn} />
+        <input
+          type="submit"
+          value="Log In"
+          className={styles.signInBtn}
+          disabled={hasErrors}
+        />
       </form>
     );
   }
